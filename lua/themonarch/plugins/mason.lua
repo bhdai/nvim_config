a5f64fd3b8a6bcb36f861f41240de9cb8262308e
buidai123 <ss1280dzzz@gmail.com>
2024-09-09 10:35:54 +0700

    update config

--- lua/themonarch/plugins/mason.lua
+++ lua/themonarch/plugins/mason.lua
@@ -1,42 +1,42 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    require("mason").setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-        }
-      }
-    })
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    require("mason-lspconfig").setup({
-      automatic_installation = true,
-      ensure_installed = {
-        "cssls",
-        "eslint",
-        "html",
-        "jsonls",
-        "tsserver",
-        "pyright",
-        "tailwindcss",
-      },
-    })
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				"cssls",
+				"eslint",
+				"html",
+				"jsonls",
+				-- "tsserver",
+				"pyright",
+				"tailwindcss",
+			},
+		})
 
-    require("mason-tool-installer").setup({
-      ensure_installed = {
-        "prettier",
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
-      },
-    })
-  end,
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"prettier",
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint",
+				"eslint_d",
+			},
+		})
+	end,
 }
