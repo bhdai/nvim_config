fede86f294757d619901c9586dec9b458e19b7a9
buidai123 <ss1280dzzz@gmail.com>
2024-10-11 10:55:37 +0700

    feat(codding): add nvim-dap and nvim-navic plugn

--- lua/themonarch/plugins/coding.lua
+++ lua/themonarch/plugins/coding.lua
@@ -240,4 +240,140 @@ return {
 			end
 		end,
 	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-lua/plenary.nvim",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			dapui.setup()
+
+			-- Automatically open UI when debugging starts
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+
+			-- Configure virtual text
+			require("nvim-dap-virtual-text").setup()
+
+			-- Set up signs
+			vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
+
+			-- Keymappings
+			vim.keymap.set("n", "<leader>d", "")
+			vim.keymap.set("n", "<leader>dB", function()
+				dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+			end, { desc = "Breakpoint Condition" })
+			vim.keymap.set("n", "<leader>db", function()
+				dap.toggle_breakpoint()
+			end, { desc = "Toggle Breakpoint" })
+			vim.keymap.set("n", "<leader>dc", function()
+				dap.continue()
+			end, { desc = "Continue" })
+			vim.keymap.set("n", "<leader>di", function()
+				dap.step_into()
+			end, { desc = "Step Into" })
+			vim.keymap.set("n", "<leader>do", function()
+				dap.step_over()
+			end, { desc = "Step Over" })
+			vim.keymap.set("n", "<leader>dO", function()
+				dap.step_out()
+			end, { desc = "Step Out" })
+			vim.keymap.set("n", "<leader>dr", function()
+				dap.repl.toggle()
+			end, { desc = "Toggle REPL" })
+			vim.keymap.set("n", "<leader>dl", function()
+				dap.run_last()
+			end, { desc = "Run Last" })
+			vim.keymap.set("n", "<leader>dt", function()
+				dap.terminate()
+			end, { desc = "Terminate" })
+			vim.keymap.set("n", "<leader>dw", function()
+				require("dap.ui.widgets").hover()
+			end, { desc = "Widgets" })
+
+			-- Load launch.json if it exists
+			local vscode = require("dap.ext.vscode")
+			local json = require("plenary.json")
+			vscode.json_decode = function(str)
+				return vim.json.decode(json.json_strip_comments(str))
+			end
+			if vim.fn.filereadable(".vscode/launch.json") == 1 then
+				vscode.load_launchjs()
+			end
+
+			-- You'll need to add language-specific configurations here.
+			dap.adapters.python = {
+				type = "executable",
+				command = "python",
+				args = { "-m", "debugpy.adapter" },
+			}
+
+			dap.configurations.python = {
+				{
+					type = "python",
+					request = "launch",
+					name = "Launch file",
+					program = "${file}",
+					pythonPath = function()
+						return "python"
+					end,
+				},
+			}
+
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+					args = { "--port", "${port}" },
+				},
+			}
+
+			dap.configurations.cpp = {
+				{
+					name = "Launch file",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+				},
+			}
+
+			dap.configurations.c = dap.configurations.cpp
+		end,
+	},
+	{
+		"smiteshp/nvim-navic",
+		config = function()
+			require("nvim-navic").setup({
+				lsp = {
+					auto_attach = true,
+					-- priority order for attaching LSP servers
+					-- to the current buffer
+					preference = {
+						"html",
+						"templ",
+					},
+				},
+				separator = " 󰁔 ",
+			})
+		end,
+	},
 }
