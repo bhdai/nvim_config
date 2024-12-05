fef01dcf8b9fcdacec754a85e6e9f543f8c57da1
Bui Huu Dai <ss1280dzzz@gmail.com>
2024-12-05 21:07:54 +0700

    feat(smear-corsor): add smear-corsor.nvim

--- lua/themonarch/plugins/smear-cursor.lua
+++ lua/themonarch/plugins/smear-cursor.lua
@@ -1,14 +1,10 @@
 return {
 	"sphamba/smear-cursor.nvim",
 	opts = {
-		smear_between_buffers = true, -- smear cursor when switching buffers
-		-- Use floating windows to display smears over wrapped lines or outside buffers.
-		-- May have performance issues with other plugins.
 		use_floating_windows = true,
-
-		-- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-		-- Smears will blend better on all backgrounds.
-		legacy_computing_symbols_support = false,
+		legacy_computing_symbols_support = true,
 		distance_stop_animating = 0.5,
+		stiffness = 0.8,
+		hide_target_hack = false,
 	},
 }
