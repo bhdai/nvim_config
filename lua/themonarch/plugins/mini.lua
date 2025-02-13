68f9208169806d0f3e6453f2274798c46ba618bf
Bui Dai <ss1280dzzz@gmail.com>
2025-02-13 17:28:36 +0700

    feat(mini.comment): better comments
    support more files

--- lua/themonarch/plugins/mini.lua
+++ lua/themonarch/plugins/mini.lua
@@ -100,6 +100,14 @@ return {
 	{
 		"echasnovski/mini.comment",
 		event = "VeryLazy",
+		dependencies = {
+			{
+				"folke/ts-comments.nvim",
+				opts = {},
+				event = "VeryLazy",
+				enabled = vim.fn.has("nvim-0.10.0") == 1,
+			},
+		},
 		opts = {},
 	},
 }
