390688ccce39a4681525cc68dc36cb3fac9444a4
Bui Dai <ss1280dzzz@gmail.com>
2025-05-10 22:25:07 +0700

    feat: use treesitter context instead of navic

--- lua/themonarch/plugins/treesitter.lua
+++ lua/themonarch/plugins/treesitter.lua
@@ -71,6 +71,7 @@ return {
 				"kconfig",
 				"meson",
 				"ninja",
+				"rst",
 				"nix",
 				"org",
 				"php",
@@ -164,6 +165,25 @@ return {
 					end
 				end
 			end
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		-- event = "LazyFile",
+		opts = function()
+			local tsc = require("treesitter-context")
+			Snacks.toggle({
+				name = "Treesitter Context",
+				get = tsc.enabled,
+				set = function(state)
+					if state then
+						tsc.enable()
+					else
+						tsc.disable()
+					end
+				end,
+			}):map("<leader>ut")
+			return { mode = "cursor", max_lines = 3 }
 		end,
 	},
 }
