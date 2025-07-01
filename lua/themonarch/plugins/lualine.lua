ed6bd7033f0e46677be568c9e2634d1e6c20854c
Bui Dai <ss1280dzzz@gmail.com>
2025-07-01 10:08:58 +0700

    feat: rewrite status line

--- lua/themonarch/plugins/lualine.lua
+++ lua/themonarch/plugins/lualine.lua
@@ -1,6 +1,5 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	event = "VeryLazy",
 	dependencies = { "echasnovski/mini.icons" },
 	opts = function()
 		local utils = require("core.utils.general")
@@ -11,6 +10,16 @@ return {
 			["Warning"] = utils.get_hlgroup("DiagnosticError"),
 			["InProgress"] = utils.get_hlgroup("DiagnosticWarn"),
 		}
+
+		local function hl_by_state(default_hl)
+			local name = vim.bo.modified and "WarningMsg" or vim.bo.readonly and "ErrorMsg" or default_hl
+			local hl = utils.get_hlgroup(name, nil)
+			hl.gui = "bold"
+			if name == default_hl then
+				hl.fg = "#ffffff"
+			end
+			return hl
+		end
 
 		return {
 			options = {
@@ -28,10 +37,27 @@ return {
 						fmt = function(mode)
 							return mode:lower()
 						end,
+						color = {
+							gui = "bold",
+						},
 					},
 				},
-				lualine_b = { { "branch", icon = "" } },
+				lualine_b = { { "branch", icon = "" } },
 				lualine_c = {
+					{
+						function()
+							local root = utils.get_root({ detectors = { "pattern" } })
+							local name = vim.fn.fnamemodify(root, ":t")
+							-- return " " .. name
+							return "󰂖 " .. name
+						end,
+						cond = function()
+							return utils.get_root({ detectors = { "pattern" } }) ~= nil
+						end,
+						color = utils.get_hlgroup("Directory", nil),
+						padding = { left = 0, right = 1 },
+						separator = { right = "" },
+					},
 					{
 						"diagnostics",
 						symbols = {
@@ -40,37 +66,86 @@ return {
 							info = icons.diagnostics.Info,
 							hint = icons.diagnostics.Hint,
 						},
+						separator = "",
 					},
-					{ "filetype", icon_only = true, separator = "", padding = { left = 0, right = 0 } },
+					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					{
+						function()
+							local file_path = vim.api.nvim_buf_get_name(0)
+							if file_path == "" then
+								return ""
+							end
+
+							if vim.fn.fnamemodify(file_path, ":p:h") == vim.loop.cwd() then
+								return ""
+							end
+
+							local root = utils.get_root({ detectors = { "pattern" } })
+							local file_dir
+							if root then
+								local rel_path = vim.fs.normalize(file_path):gsub(vim.fs.normalize(root), "")
+								if rel_path:sub(1, 1) == "/" then
+									rel_path = rel_path:sub(2)
+								end
+								file_dir = vim.fn.fnamemodify(rel_path, ":h")
+							else
+								file_dir = vim.fn.fnamemodify(file_path, ":.:h")
+							end
+
+							if file_dir == "." or file_dir == "" then
+								return ""
+							end
+
+							local shortened_path = vim.fn.pathshorten(file_dir)
+							if shortened_path == "." then
+								return ""
+							end
+
+							return shortened_path .. "/"
+						end,
+						padding = { left = 0, right = 0 },
+						color = { fg = utils.get_hlgroup("Comment").fg },
+						separator = "",
+					},
 					{
 						"filename",
+						path = 0, -- only show the filename
 						padding = { left = 0, right = 0 },
-            color = { gui = "bold" },
+						color = function()
+							return hl_by_state("LualineFilename")
+						end,
+						symbols = {
+							modified = "", -- already handled by hl_by_state color
+							readonly = "󰦝",
+							unnamed = " [No Name]",
+						},
 					},
 					{
 						function()
-							local buffer_count = require("core.utils.general").get_buffer_count()
-
-							return "+" .. buffer_count - 1 .. " "
+							local n = utils.get_buffer_count() - 1
+							return (n > 0 and ("  " .. n) or "")
 						end,
 						cond = function()
-							return require("core.utils.general").get_buffer_count() > 1
+							return utils.get_buffer_count() > 1
 						end,
 						color = utils.get_hlgroup("Operator", nil),
-						padding = { left = 0, right = 1 },
+						padding = { left = 0, right = 0 },
 					},
 					{
 						function()
-							local tab_count = vim.fn.tabpagenr("$")
-							if tab_count > 1 then
-								return vim.fn.tabpagenr() .. " of " .. tab_count
-							end
+							local cur = vim.fn.tabpagenr()
+							local total = vim.fn.tabpagenr("$")
+							return total > 1 and (cur .. "/" .. total) or ""
 						end,
 						cond = function()
 							return vim.fn.tabpagenr("$") > 1
 						end,
-						icon = "󰓩",
-						color = utils.get_hlgroup("Special", nil),
+						icon = "",
+						color = function()
+							local total = vim.fn.tabpagenr("$")
+							return total > 5 and utils.get_hlgroup("WarningMsg", nil)
+								or utils.get_hlgroup("Special", nil)
+						end,
 					},
 				},
 				lualine_x = {
@@ -96,16 +171,46 @@ return {
 							return copilot_colors[status.status] or copilot_colors[""]
 						end,
 					},
-					{ "diff" },
+					{
+						"diff",
+						symbols = {
+							added = icons.git.added,
+							modified = icons.git.modified,
+							removed = icons.git.removed,
+						},
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
 				},
 				lualine_y = {
-					{ "progress", padding = { left = 1, right = 1 } },
-					{ "location", padding = { left = 0, right = 1 } },
+					{
+						function()
+							local current_line = vim.fn.line(".")
+							local total_lines = vim.fn.line("$")
+							local percent = math.floor((current_line / total_lines) * 100)
+							return " " .. percent .. "%%"
+						end,
+						padding = { left = 1, right = 0 },
+					},
 				},
 				lualine_z = {
-					function()
-						return " " .. os.date("%R")
-					end,
+					{
+						function()
+							return " " .. vim.fn.line(".") .. "|" .. vim.fn.col(".")
+						end,
+						padding = { left = 1, right = 1 },
+						color = {
+							gui = "bold",
+						},
+					},
 				},
 			},
 			extensions = { "fzf", "mason", "lazy" },
